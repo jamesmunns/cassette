7c3679870981fa3e3ab64bb1672ca97b69b0d96b
James Munns <james.munns@ferrous-systems.com>
2021-04-18 18:11:43 +0200

    think more about async

--- src/main.rs
+++ src/main.rs
@@ -1,96 +1,49 @@
-#![allow(dead_code)]
+#![allow(dead_code, unused_imports, unused_variables)]
 
-// TODO: How could I make sure that context items used by
-// a state are always initialized or in the right state?
+use futures::{
+    pin_mut,
+    future::poll_fn,
+};
 
 struct Demo {
     lol: u32,
 }
 
-// Are these "non-leaf states"?
-enum DemoTopStates {
-    ZeroToTen,
-    FiveToZero
-}
-
-// Are these "leaf states"?
-enum DemoBottomStates {
-    StartAtZero,
-    StartAtFive,
-    AddOneUntilTen,
-    SubOneUntilZero,
-}
-
-trait Queue {
-    type Item;
-    fn peek(&self) -> Option<fn(&mut Self::Item) -> Result<bool, ()>>;
-    fn pop(&mut self);
-    fn resolve(self) -> Self;
-}
-
-struct SimpQueue<T: 'static> {
-    queue: &'static [fn(&mut T) -> Result<bool, ()>],
-    on_success: &'static [fn(&mut T) -> Result<bool, ()>],
-}
-
-trait Cassette<Q: Queue<Item = Self>> {
-    type Item;
-    fn set_queue(&mut self, q: Q);
-    fn get_queue(&mut self) -> &mut Q;
-    fn poll(&mut self) -> Result<(), ()>;
-}
-
-type BottomSequence = &'static [fn(&mut Demo) -> Result<bool, ()>];
-type TopSequence = &'static [BottomSequence];
-
-const ZERO_TO_TEN: BottomSequence = &[
-    Demo::start_at_zero,
-    Demo::add_one_until_ten,
-];
-
-const ZERO_TO_ZERO: BottomSequence = &[
-    Demo::start_at_zero,
-    Demo::add_one_until_ten,
-    Demo::sub_one_until_zero,
-];
-
 impl Demo {
-    fn zero_to_ten(&self) -> TopSequence {
-        &[ZERO_TO_TEN]
+    async fn entry(&mut self) -> Result<bool, ()> {
+        self.start_at_zero().await;
+        self.add_one_until_ten().await;
+        self.sub_one_until_zero().await;
+        Ok(true)
     }
 
-    fn zero_to_zero(&self) -> TopSequence {
-        &[ZERO_TO_ZERO]
-    }
 
-    fn fancy(&self) -> TopSequence {
-        &[
-            ZERO_TO_TEN,
-            ZERO_TO_ZERO,
-            ZERO_TO_TEN,
-        ]
-    }
-}
-
-impl Demo {
-    fn start_at_zero(&mut self) -> Result<bool, ()> {
+    async fn start_at_zero(&mut self) {
         self.lol = 0;
-        Ok(true)
     }
 
-    fn start_at_five(&mut self) -> Result<bool, ()> {
+    async fn start_at_five(&mut self) {
         self.lol = 5;
-        Ok(true)
     }
 
-    fn add_one_until_ten(&mut self) -> Result<bool, ()> {
-        self.lol += 1;
-        Ok(self.lol >= 10)
+    async fn add_one_until_ten(&mut self) {
+        loop {
+            cats(self).await; // simulate fake delays/not ready state
+            self.lol += 1;
+            if self.lol >= 10 {
+                return
+            }
+        }
     }
 
-    fn sub_one_until_zero(&mut self) -> Result<bool, ()> {
-        self.lol -= 1;
-        Ok(self.lol == 0)
+    async fn sub_one_until_zero(&mut self) {
+        loop {
+            cats(self).await; // simulate fake delays/not ready state
+            self.lol -= 1;
+            if self.lol == 0 {
+                return
+            }
+        }
     }
 }
 
@@ -130,16 +83,14 @@ static RWVT: RawWakerVTable = RawWakerVTable::new(
 
 fn main() {
     let mut demo = Demo { lol: 100 };
-    let mut x = lol(&mut demo);
+    let x = demo.entry();
+    pin_mut!(x);
 
     let raw_waker = RawWaker::new(core::ptr::null(), &RWVT);
     let waker = unsafe { Waker::from_raw(raw_waker) };
 
     loop {
-        let y = Future::poll(
-            unsafe { Pin::new_unchecked(&mut x) },
-            &mut Context::from_waker(&waker)
-        );
+        let y = x.as_mut().poll(&mut Context::from_waker(&waker));
         match y {
             Poll::Pending => { },
             Poll::Ready(yes) => {
@@ -147,10 +98,6 @@ fn main() {
                 break;
             }
         }
-        // match lol(&mut demo).poll(()) {
-        //     Poll::Pending => todo!(),
-        //     Poll::Ready(stuff) => todo!()
-        // }
     }
 }
 
@@ -159,21 +106,13 @@ use std::sync::{
     atomic::Ordering,
 };
 
-async fn lol(ctxt: &mut Demo) -> Result<bool, ()> {
-    println!("boots!");
-    cats(ctxt).await?;
-    cats(ctxt).await?;
-    cats(ctxt).await?;
-    Ok(true)
-}
-
 static FAKE: AtomicU32 = AtomicU32::new(0);
 
-async fn cats(ctxt: &mut Demo) -> Result<bool, ()> {
+async fn cats(ctxt: &mut Demo) {
+    println!("cats says lol: {}", ctxt.lol);
     let x = CountFuture;
     x.await;
     println!("and cats!");
-    Ok(false)
 }
 
 struct CountFuture;
@@ -182,7 +121,7 @@ impl Future for CountFuture {
     type Output = ();
     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
         let x = FAKE.fetch_add(1, Ordering::SeqCst);
-        println!("    ...{}", x);
+        print!("{}, ", x);
         if (x % 5) == 0 {
             Poll::Ready(())
         } else {
