7578588953a68ffdf0938619d6e56aa4bbc5f9e4
James Munns <james.munns@ferrous-systems.com>
2021-04-18 21:37:40 +0200

    Steal pin mut macro to not require `futures` dep

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,35 @@ use core::{
     task::{Context, Poll, RawWaker, RawWakerVTable, Waker},
 };
 
+/// Pins a value on the stack.
+///
+/// NOTE: Taken from `futures::pin_mut!()`
+///
+/// # Example
+///
+/// ```rust
+/// # use pin_utils::pin_mut;
+/// # use core::pin::Pin;
+/// # struct Foo {}
+/// let foo = Foo { /* ... */ };
+/// pin_mut!(foo);
+/// let _: Pin<&mut Foo> = foo;
+/// ```
+#[macro_export]
+macro_rules! pin_mut {
+    ($($x:ident),* $(,)?) => { $(
+        // Move the value to ensure that it is owned
+        let mut $x = $x;
+        // Shadow the original binding so that it can't be directly accessed
+        // ever again.
+        #[allow(unused_mut)]
+        let mut $x = unsafe {
+            core::pin::Pin::new_unchecked(&mut $x)
+        };
+    )* }
+}
+
+
 unsafe fn fake_clone(_: *const ()) -> RawWaker {
     todo!()
 }
